12b8ce01fa34a4a283effc2f3921eb98b83830bb
Zachary Groff <groff.zackm@gmail.com>
2017-12-08 11:32:00 -0500

    Add files via upload

---
+++ star16.py
@@ -0,0 +1,61 @@
+myList = []
+registerValues = dict()
+maxValue = 0
+
+with open('day8input') as lines:
+    for line in lines:
+        myList.append(line.split())
+
+#set all registers to 0
+for i in myList:
+    registerValues[i[0]] = 0
+
+for i in myList:    
+    if i[5] == '!=':
+        if registerValues[i[4]] != int(i[6]):
+            if i[1] == 'inc':
+                registerValues[i[0]] += int(i[2])
+            elif i[1] == 'dec':
+                registerValues[i[0]] -= int(i[2]) 
+    
+    elif i[5] == '>':
+        if registerValues[i[4]] > int(i[6]):
+            if i[1] == 'inc':
+                registerValues[i[0]] += int(i[2])
+            elif i[1] == 'dec':
+                registerValues[i[0]] -= int(i[2]) 
+   
+    elif i[5] == '>=':
+        if registerValues[i[4]] >= int(i[6]):
+            if i[1] == 'inc':
+                registerValues[i[0]] += int(i[2])
+            elif i[1] == 'dec':
+                registerValues[i[0]] -= int(i[2]) 
+    
+    elif i[5] == '<':        
+        if registerValues[i[4]] < int(i[6]):
+            if i[1] == 'inc':
+                registerValues[i[0]] += int(i[2])
+            elif i[1] == 'dec':
+                registerValues[i[0]] -= int(i[2]) 
+    
+    elif i[5] == '<=':
+        if registerValues[i[4]] <= int(i[6]):
+            if i[1] == 'inc':
+                registerValues[i[0]] += int(i[2])
+            elif i[1] == 'dec':
+                registerValues[i[0]] -= int(i[2]) 
+    
+    elif i[5] == '==':
+        if registerValues[i[4]] == int(i[6]):
+            if i[1] == 'inc':
+                registerValues[i[0]] += int(i[2])
+            elif i[1] == 'dec':
+                registerValues[i[0]] -= int(i[2]) 
+    else:
+        print('error', i[5])
+    
+    if registerValues[i[0]] > maxValue:
+        maxValue = registerValues[i[0]]
+
+print(maxValue)
